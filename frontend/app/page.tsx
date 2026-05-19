77f44e5c5ef3db5213c3e8b7a05e3bb454415de4
zyuuuukak1n <zyuuuukak1n@gmail.com>
2026-05-19 14:40:43 +0900

    update

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,7 @@
 // frontend/app/page.tsx
 export default function Home() {
     return (
-        <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: '1a1a1a', color: 'white'}}>
+        <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: '#1a1a1a', color: 'white'}}>
           <h1>フロントエンド正常稼働中</h1>
         </main>
     );
